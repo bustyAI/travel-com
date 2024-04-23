44f00813d489559ef7e4dad39d2aa9088eb52264
bustyAI <oscar120646@gmail.com>
2024-04-23 16:29:40 -0700

    removing defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      width: {
+        "app-width": "1440px"
       },
+      maxWidth: {
+        '1440': "1440px"
+      }
     },
   },
   plugins: [],
